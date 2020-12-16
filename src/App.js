a1afa2932ff6cf00442225f6eb45660c8ecaea51
Fedenko Maksym <feden2906@gmail.com>
2020-12-16 23:43:40 +0200

    ---

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Header from "./components/header/Header";
+import Body from "./components/body/Body";
+import {BrowserRouter as Router} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <Header/>
+          <Body/>
+        </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/buttons/edit/Edit.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './Edit.css'
+
+export class Edit extends Component {
+  render() {
+    return (
+          <button className='btn-edit'>Edit</button>
+    );
+  }
+}
+
