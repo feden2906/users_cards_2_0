97f173a96d4d700c11402438d8cf68825b57bdf6
Fedenko Maksym <feden2906@gmail.com>
2021-01-09 23:02:22 +0200

    style

--- src/components/header-item/HeaderItem.js
+++ src/components/header-item/HeaderItem.js
@@ -3,8 +3,10 @@ import './HeaderItem.css'
 import {Link} from "react-router-dom";
 
 class HeaderItem extends Component {
+
   render() {
     const {item} = this.props
+
     return (
         <div className='header-item'>
           <Link to={'/' + item}><b>{item}</b></Link>
