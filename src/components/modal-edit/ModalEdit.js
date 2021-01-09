acc1c22a00b982200b9390e7367ee276f42adf25
Fedenko Maksym <feden2906@gmail.com>
2021-01-09 23:05:05 +0200

    style

--- src/components/modal-edit/ModalEdit.js
+++ src/components/modal-edit/ModalEdit.js
@@ -21,26 +21,32 @@ class ModalEdit extends Component {
         <div className='modal-wrapper'>
           <div className='modal-form'>
             <h2><b>Edit form</b></h2>
+
             <div className='modal-input-wrapper'>
               <span><b className='fs20'>Name :</b></span>
               <input onInput={this.checkName} className='modal-input' type="text" defaultValue={name}/>
             </div>
+
             <div className='modal-input-wrapper'>
               <span><b className='fs20'>Username :</b></span>
               <input onInput={this.checkUserName} className='modal-input' type="text" defaultValue={username}/>
             </div>
+
             <div className='modal-input-wrapper'>
               <span><b className='fs20'>Phone :</b></span>
               <input onInput={this.checkPhone} className='modal-input' type="text" defaultValue={phone}/>
             </div>
+
             <div className='modal-input-wrapper'>
               <span><b className='fs20'>Email :</b></span>
               <input onInput={this.checkEmail} className='modal-input' type="text" defaultValue={email}/>
             </div>
+
             <div className='modal-input-wrapper'>
               <span><b className='fs20'>Website :</b></span>
               <input onInput={this.checkWebSite} className='modal-input' type="text" defaultValue={website}/>
             </div>
+
             <div className='modal-btn-wrapper'>
               <button onClick={() => modalSaveClose('save', this.state)} className='modal-btn'>Save</button>
               <button onClick={() => modalSaveClose()} className='modal-btn'>Close</button>
